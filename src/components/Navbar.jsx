c82095bb0d2f0ca4a787ea220fef0eb75cdc5906
Andrii <shapovala@ukr.net>
2025-11-30 11:27:20 +0200

    added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import Images from './Images';
+import { Link } from 'react-router-dom';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
     <div className='w-full h-16 md:h-20 flex items-center justify-between'>
       {/* Logo */}
-      <div className='flex items-center gap-4 text-2xl font-bold'>
+      <Link to="/" className='flex items-center gap-4 text-2xl font-bold'>
         <Images src= 'https://ik.imagekit.io/o31ofkqc7/logo.png' alt='lama logo' w={32} h={32} />
         <span>lamalog</span>
-      </div>
+      </Link>
       {/* Mobile menu */}
       <div className='md:hidden'>
         <div className='cursor-pointer text-4xl' onClick={()=> setOpen(prev=>!prev)}>
@@ -17,24 +19,29 @@ const Navbar = () => {
         </div>
         {/* MobileLink List */}
         <div className={`w-full h-screen flex flex-col items-center gap-8 font-medium text-lg justify-center absolute top-16 transition-all ease-in-out ${open ? "-right-0" :"-right-[100%]"}`}>
-          <a href='/'>Home</a>
-          <a href='/'>Trending</a>
-          <a href='/'>Most popular</a>
-          <a href='/'>About</a>
-          <a href='/'>
+          <Link to="/">Home</Link>
+          <Link to="/">Trending</Link>
+          <Link to="/">Most popular</Link>
+          <Link to="/">About</Link>
+          <Link to="/">
             <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login ✋</button>
-          </a>
+          </Link>
         </div>
       </div>
       {/* Desktop menu */}
       <div className='hidden md:flex items-center gap-8 xl:gap-12 font-medium'>
-        <a href='/'>Home</a>
-        <a href='/'>Trending</a>
-        <a href='/'>Most popular</a>
-        <a href='/'>About</a>
-        <a href='/'>
-          <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login ✋</button>
-        </a>
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most popular</Link>
+        <Link to="/">About</Link>
+        <SignedOut>
+          <Link to="/login">
+            <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login ✋</button>
+          </Link>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </div>
   )
