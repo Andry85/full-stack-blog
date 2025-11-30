723d1a000af0c94bb4f85f63b31425e1aa2919c3
Andrii <shapovala@ukr.net>
2025-11-30 19:44:31 +0200

    added files

---
+++ src/components/MainCategories.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const MainCategories = () => {
+  return (
+    <div className='hidden md:flex bg-white rounded-3xl xl:rounded-full p-4 shadow-lg items-center justify-center gap-8'>
+        <div className='flex-1 flex items-center justify-between flex-wrap'>
+            <Link to="/post" className='bg-blue-800 text-white rounded-full px-4 py-2'>All posts</Link>
+            <Link to="/post?cat=web-design" className='hover:bg-blue-500 rounded-full px-4 py-2'>Web Design</Link>
+            <Link to="/post?cat=development" className='hover:bg-blue-500 rounded-full px-4 py-2'>Development</Link>
+            <Link to="/post?cat=web-design" className='hover:bg-blue-500 rounded-full px-4 py-2'>Databases</Link>
+            <Link to="/post?cat=web-design" className='hover:bg-blue-500 rounded-full px-4 py-2'>Search Engines</Link>
+            <Link to="/post?cat=web-design" className='hover:bg-blue-500 rounded-full px-4 py-2'>Marketing</Link>
+        </div>
+        <div className='search'>
+            Search
+        </div>
+    </div>
+  )
+}
+
+export default MainCategories
