7d7dbd24750954d7d76cd497139b330c17056189
Andrii <shapovala@ukr.net>
2025-12-17 15:52:10 +0200

    added menu

--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Images from '../components/Images';
 import { Link } from 'react-router-dom'
+import PostMenuActions from '../components/PostMenuActions';
+import Search from '../components/Search';
 
 
 const SinglePostPage = () => {
@@ -26,8 +28,51 @@ const SinglePostPage = () => {
           <div className='lg:text-lg flex flex-col gap-6 text-justify'>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, asperiores quasi, quia odit saepe sunt ratione consequatur maiores dolorum tempore est voluptatem voluptatum distinctio repellendus cum! Impedit, earum fugiat. Vitae?</p>
           </div>
-          <div className=''></div>
+          <div className='px-4 h-max sticky top-8'>
+            <h1 className='mb-4 text-sm font-medium'>Author</h1>
+            <div className='flex flex-col gap-4'>
+              <div className='flex items-center gap-8'>
+                <Images src="userImg.jpeg" className="w-12 h-12 rounded-full object-cover" w="48" h="48" />
+                <Link>John Doe</Link>
+              </div>
+              <p className='text-sm text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+              <div className='flex gap-2'>
+                <Link>
+                  <Images src="facebook.svg"/>
+                </Link>
+                <Link>
+                  <Images src="instagram.svg"/>
+                </Link>
+              </div>
+            </div>
+            <PostMenuActions/>
+            <h1 className='mt-8 mb-4 text-sm font-medium'>Categories</h1>
+            <div className='flex flex-col gap-2 text-sm'>
+              <Link className='underline'>All</Link>
+              <Link to="/post?cat=web-design">Web Design</Link>
+              <Link to="/post?cat=development">Development</Link>
+              <Link to="/post?cat=web-design">Databases</Link>
+              <Link to="/post?cat=web-design">Search Engines</Link>
+              <Link to="/post?cat=web-design">Marketing</Link>
+            </div>
+            <h1 className='mt-8 mb-4 text-sm font-medium'>Search</h1>
+            <Search/>
+          </div>
       </div>
     </div>
   )
